05f9aa6e9aa8fd31d23fbd9b60eab454df225ecf
joangnz <joangnz@gmail.com>
2025-03-18 18:24:34 +0100

    Format of Subjecs List

--- src/components/Lists/SubjectsList.tsx
+++ src/components/Lists/SubjectsList.tsx
@@ -17,21 +17,19 @@ export default function SubjectsList() {
                 Authorization: "Bearer" + localStorage.getItem("token"),
             },
         })
-        .then((res) => res.json())
-        .then((data) => setSubjects(data))
-        .catch(() => setError("Error al obtener las asignaturas"));
+            .then((res) => res.json())
+            .then((data) => setSubjects(data))
+            .catch(() => setError("Error al obtener las asignaturas"));
     }, []);
-    
+
     return (
         <div className="mt-4">
             <h2 className="text-2xl font-bold mb-2">Asignaturas</h2>
             {error && <p className="text-red-500">{error}</p>}
             <ul className="space-y-2">
                 {subjects.map((subject) => (
-                    <li key={subject.id} className="p-2 border rounded
-bg-white shadow">
-                        <strong>{subject.name}</strong> - ID Curso:
-                        {subject.course_id}
+                    <li key={subject.id} className="p-2 border rounded bg-white shadow">
+                        <strong>{subject.name}</strong> - ID Curso: {subject.course_id}
                     </li>
                 ))}
             </ul>
