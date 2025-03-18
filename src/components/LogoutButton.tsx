28fe8b3bd7f7c66c32cc43a70d01f196bf6cb8b2
joangnz <joangnz@gmail.com>
2025-03-18 19:13:45 +0100

    Edited classNames as per Original Task Code

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -4,7 +4,7 @@ interface LogoutButtonProps {
 
 export default function LogoutButton({ onLogout }: LogoutButtonProps) {
     return (
-        <button onClick={onLogout} className="bg-red-600 text-white p-2 rounded hover:bg-red500" >
+        <button onClick={onLogout} className="mt-4 bg-red-600 text-white p-2 rounded hover:bg-red-500" >
             Logout
         </button>
     )
